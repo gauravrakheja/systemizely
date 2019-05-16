0754e01beabcae5f6b5c352f8cca7273d853bd07
gaurav-rakheja <gaurav.rakheja@go-jek.com>
2019-05-17 02:38:37 +0530

    adds link to view wishlist item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def with_protocol(url)
+    if url && !url_protocol_present?(url)
+      "http://#{url}"
+    else
+      url
+    end
+  end
+
+  def url_protocol_present?(url)
+    url[/\Ahttp:\/\//] || url[/\Ahttps:\/\//]
+  end
 end
